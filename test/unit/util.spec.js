d74f394beb4687f66dab81d338e4f095d8b3019b
Luke Turner <github@luketurner.org>
2019-11-22 17:25:30 -0800

    Delete outdated tests

--- test/unit/util.spec.js
+++
@@ -1,6 +0,0 @@
-const expect = require("chai").expect;
-const sinon = require("sinon");
-require("chai").use(require("sinon-chai"));
-const util = require("../../src/util");
-
-describe("module: util", () => {});
