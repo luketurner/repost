d126821a7c74b0703de97999329c3a734aa050b3
Luke Turner <github@luketurner.org>
2019-11-22 17:33:58 -0800

    Add support for printing results from custom JS handlers... kinda

--- src/cli/print.js
+++ src/cli/print.js
@@ -1,4 +1,8 @@
 const httpResponse = response => {
+  if (!response.request) {
+    // TODO -- not an http response, probably from a custom JS thing. Just assume they set .data
+    return response.data;
+  }
   return (
     `${response.request.method} ${response.config.url}\n` + // TODO -- URL doesn't work
     Object.entries(response.headers)
